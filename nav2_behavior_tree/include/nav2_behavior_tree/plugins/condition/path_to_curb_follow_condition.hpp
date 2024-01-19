12d85cc600c57547d9aa92b1a8abea0862da8577
Onicc <xinchen.cai@hotmail.com>
2024-01-19 11:06:45 +0800

    Add a condition node for switching to curb following mode in the path-following mode.

---
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/condition/path_to_curb_follow_condition.hpp
@@ -0,0 +1,76 @@
+// Copyright (c) 2020 Aitor Miguel Blanco
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__PATH_TO_CURB_FOLLOW_CONDITION_HPP_
+#define NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__PATH_TO_CURB_FOLLOW_CONDITION_HPP_
+
+#include <string>
+#include <vector>
+
+#include "behaviortree_cpp_v3/condition_node.h"
+#include "nav_msgs/msg/path.hpp"
+#include "nav2_msgs/msg/waypoint.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
+namespace nav2_behavior_tree
+{
+
+/**
+ * @brief A BT::ConditionNode that returns SUCCESS when goal is
+ * updated on the blackboard and FAILURE otherwise
+ */
+class PathToCurbFollowCondition : public BT::ConditionNode
+{
+public:
+  /**
+   * @brief A constructor for nav2_behavior_tree::PathToCurbFollowCondition
+   * @param condition_name Name for the XML tag for this node
+   * @param conf BT node configuration
+   */
+  PathToCurbFollowCondition(
+    const std::string & condition_name,
+    const BT::NodeConfiguration & conf);
+
+  PathToCurbFollowCondition() = delete;
+
+  /**
+   * @brief The main override required by a BT action
+   * @return BT::NodeStatus Status of tick execution
+   */
+  BT::NodeStatus tick() override;
+
+  /**
+   * @brief Creates list of BT ports
+   * @return BT::PortsList Containing node-specific ports
+   */
+  static BT::PortsList providedPorts()
+  {
+    return {
+      BT::InputPort<nav_msgs::msg::Odometry>("odometry_gps", "GPS position"),
+      BT::InputPort<double>("max_position_covariance", 1.0, "Maximum value of GPS position covariance"),
+      BT::InputPort<double>("max_angle_covariance", 1.0, "Maximum value of GPS angle covariance"),
+      BT::InputPort<nav2_msgs::msg::Waypoint>("waypoint", "The nearest waypoint"),
+      BT::InputPort<geometry_msgs::msg::PoseStamped>("curb_traction_point", "Curb traction points in the odom frame"),
+    };
+  }
+  
+
+private:
+  geometry_msgs::msg::PoseStamped last_curb_traction_point_;
+
+};
+
+}  // namespace nav2_behavior_tree
+
+#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__GOAL_UPDATED_CONDITION_HPP_
---
+++ nav2_behavior_tree/plugins/condition/path_to_curb_follow_condition.cpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2020 Aitor Miguel Blanco
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+#include <vector>
+#include "nav2_behavior_tree/plugins/condition/path_to_curb_follow_condition.hpp"
+
+namespace nav2_behavior_tree
+{
+
+PathToCurbFollowCondition::PathToCurbFollowCondition(
+  const std::string & condition_name,
+  const BT::NodeConfiguration & conf)
+: BT::ConditionNode(condition_name, conf)
+{}
+
+BT::NodeStatus PathToCurbFollowCondition::tick()
+{
+  nav_msgs::msg::Odometry odometry_gps;
+  double max_position_covariance;
+  double max_angle_covariance;
+  nav2_msgs::msg::Waypoint waypoint;
+  geometry_msgs::msg::PoseStamped curb_traction_point;
+
+  getInput("odometry_gps", odometry_gps);
+  getInput("max_position_covariance", max_position_covariance);
+  getInput("max_angle_covariance", max_angle_covariance);
+  getInput("waypoint", waypoint);
+  getInput("curb_traction_point", curb_traction_point);
+
+  // Check if the odometry is valid.
+  if(odometry_gps.pose.covariance[0] > max_position_covariance ||
+    odometry_gps.pose.covariance[7] > max_position_covariance ||
+    odometry_gps.pose.covariance[14] > max_position_covariance ||
+    odometry_gps.pose.covariance[21] > max_angle_covariance ||
+    odometry_gps.pose.covariance[28] > max_angle_covariance ||
+    odometry_gps.pose.covariance[35] > max_angle_covariance) {
+    return BT::NodeStatus::FAILURE;
+  }
+
+  // Check if the curb following option for the waypoint points is turned on.
+  if(waypoint.option_curb_traction_fix == false) {
+    return BT::NodeStatus::FAILURE;
+  }
+
+  // Check if the real-time curb traction points are being updated.
+  if(curb_traction_point == last_curb_traction_point_) {
+    return BT::NodeStatus::FAILURE;
+  }
+  last_curb_traction_point_ = curb_traction_point;
+
+  return BT::NodeStatus::SUCCESS;
+}
+
+}  // namespace nav2_behavior_tree
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<nav2_behavior_tree::PathToCurbFollowCondition>("PathToCurbFollow");
+}
