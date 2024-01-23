f8007314c0a80b6835809c268fe15683c4a52004
Onicc <xinchen.cai@hotmail.com>
2024-01-23 11:05:32 +0800

    fix node name

--- nav2_behavior_tree/plugins/condition/waypoints_updated_condition.cpp
+++ nav2_behavior_tree/plugins/condition/waypoints_updated_condition.cpp
@@ -48,5 +48,5 @@ BT::NodeStatus WaypointsUpdated::tick()
 #include "behaviortree_cpp_v3/bt_factory.h"
 BT_REGISTER_NODES(factory)
 {
-  factory.registerNodeType<nav2_behavior_tree::WaypointsUpdated>("WaypointUpdated");
+  factory.registerNodeType<nav2_behavior_tree::WaypointsUpdated>("WaypointsUpdated");
 }
