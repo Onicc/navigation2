e065b1f6cbb8f50bee187bea803ce938e4cdd37b
Onicc <xinchen.cai@hotmail.com>
2025-06-12 18:59:48 +0800

    Add ToggleForwardBackwardMode to AutomaticMode and revert robot frame in blackboard to 'base_link'

--- nav2_bt_navigator/src/navigators/navigate_to_path.cpp
+++ nav2_bt_navigator/src/navigators/navigate_to_path.cpp
@@ -170,7 +170,7 @@ NavigateToPathNavigator::configure(
     node->declare_parameter("robot_frame_blackboard_id", std::string("robot_frame"));
   }
   robot_frame_blackboard_id_ = node->get_parameter("robot_frame_blackboard_id").as_string();
-  blackboard->set<std::string>(robot_frame_blackboard_id_, "base_footprint");
+  blackboard->set<std::string>(robot_frame_blackboard_id_, "base_link");
 
   if (!node->has_parameter("base_link_frame_id")) {
     node->declare_parameter("base_link_frame_id", std::string("base_link_frame"));
